7662f80da2fc775ce1f2f09f0779760b3c91d22b
Anastasiia <humenyuknastia.9616@gmail.com>
2024-05-15 13:19:03 +0200

    create app on react

---
+++ advice_app_react/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">@humenyuk16</a>.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
